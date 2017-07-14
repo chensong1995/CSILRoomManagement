4c508a5ea1ea99eb1ed8e31554edb3a2714037bf
Chen Song <csa102@sfu.ca>
2017-07-14 13:34:52 -0700

    users can turn on/off email notifications in profile page

--- webroot/js/profile.js
+++ webroot/js/profile.js
@@ -1,19 +1,21 @@
 /*
  * Author(s)  : Chen Song
  * Description: The client program for the profile page
- * Last Update: July 13, 2017
+ * Last Update: July 14, 2017
 */
 $(document).ready(function () {
     $('#update-info').on('click', function () {
-        $.post("/profile", { // post the username and password
-            biography: $("#biography").val(),
-            _csrf: $("#csrfToken").val()
+        $('#prompt').text('processing...');
+        var notification = $('#notification').is(':checked') ? 1 : 0;
+        $.post("/profile", { // post the notification and biography
+            notification: notification,
+            biography: $('#biography').val(),
+            _csrf: $('#csrfToken').val()
         },
         function () {
             $('#prompt').text('update success');
         }).fail(function () {
-            var prompt = 'update fails';
-            $('#prompt').text(prompt);
+            $('#prompt').text('update fails');
         });
     });
 });
