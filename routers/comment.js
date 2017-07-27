eb0548f6943faaa0e6833fef9d5c421ce97f602c
ruimingj <ruimingj@sfu.ca>
2017-07-26 21:48:39 -0700

    Added comment-view.pug, user can see his previous feedbacks

--- routers/comment.js
+++ routers/comment.js
@@ -26,7 +26,7 @@ router.get('/', function (req, res) {
     var username = req.userDisplay.username;
     var source = req.userDisplay.type == 'other' ? 'CSIL Account' : 'SFU Central Authentication Service';
     var allowAdmin = req.userDisplay.allowAdmin;
-    var page = "Report Violations";
+    var page = "Report Violations Send Feedback";
     // Jia, I fixed the rendering issue here. -- Chen Song
     res.render('comment', {
         username: username,
@@ -34,6 +34,42 @@ router.get('/', function (req, res) {
         allowAdmin: allowAdmin,
         page: page,
         csrfToken: req.csrfToken()
+    });
+});
+
+/*
+ * Author(s)  : Ruiming Jia
+ * Description: This function sends the feedback-view page
+ * Last Update: July 22, 2017
+*/
+router.get('/view', function (req, res) {
+    // prepare all view variables
+    var username = req.userDisplay.username;
+    var source = req.userDisplay.type == 'other' ? 'CSIL Account' : 'SFU Central Authentication Service';
+    var allowAdmin = req.userDisplay.allowAdmin;
+    var page = "Report Violations View Feedback";
+    var feedbacks = [];
+    //fetch previous feedback from database
+    req.models.Feedback.find({username: req.userDisplay.username}, function (err, results) {
+        if (err) {
+            res.sendStatus(500); // internal server error
+        } else {
+            for (var i = 0; i < results.length; i++) {
+                feedbacks.push({
+                    username: results[i].username,
+                    message: results[i].message
+                });
+            }
+            // Jia, I fixed the rendering issue here. -- Chen Song
+            res.render('comment-view', {
+                username: username,
+                source: source,
+                allowAdmin: allowAdmin,
+                page: page,
+                feedbacks: feedbacks,
+                csrfToken: req.csrfToken()
+            });
+        }
     });
 });
 
@@ -47,16 +83,18 @@ router.post('/', csrfProtection, function (req, res) {
     var message = req.body.message;
     var username = req.userDisplay.username;
     var email = req.userDisplay.email;
-    var feedbacks = [];
-        //insert message into database
+    //insert message into database
     req.models.Feedback.create({
         username: req.userDisplay.username,
         message: req.body.message
     }, function (err) {
+        var msg = err ? 'Error occured, message not sent.' : 'Message sent! Thank you.';
+        var err = err ? true : false;
         if (err) {
             res.sendStatus(500); // internal server error
         } else { // success
-            var transporter = nodemailer.createTransport({
+            
+            /*var transporter = nodemailer.createTransport({
             service: 'Gmail',
             auth: {
                 user: 'csilroombooking@gmail.com',
@@ -75,46 +113,33 @@ router.post('/', csrfProtection, function (req, res) {
             transporter.sendMail(mailOpts, function (error, response) {
                 //Email not sent
                 // Jia, I fixed the rendering issue here. -- Chen Song
+                console.log('11');
                 var source = req.userDisplay.type == 'other' ? 'CSIL Account' : 'SFU Central Authentication Service';
                 var allowAdmin = req.userDisplay.allowAdmin;
-                var page = "Report Violations";
                 var msg = error ? 'Error occured, message not sent.' : 'Message sent! Thank you.';
                 var err = error ? true : false;
-                
-                req.models.Feedback.find({username: req.userDisplay.username}, function (err, results) {
-                    if (err) {
-                        res.sendStatus(500); // internal server error
-                    } else {
-                        for (var i = 0; i < results.length; i++) {
-                            feedbacks.push({
-                                username: results[i].username,
-                                message: results[i].message
-                            });
-                        }
-                        // Jia, I fixed the rendering issue here. -- Chen Song
-                        res.render('comment', {
-                            username: username,
-                            source: source,
-                            allowAdmin: allowAdmin,
-                            page: page,
-                            feedbacks: feedbacks,
-                            csrfToken: req.csrfToken(),
-                            msg: msg,
-                            err: err
-                        });
-                    }
-                });
-                /*res.render('comment', {
+                res.render('comment', {
                     username: username,
                     source: source,
                     allowAdmin: allowAdmin,
-                    page: page,
+                    page: "Report Violations Send Feedback",
                     csrfToken: req.csrfToken(), 
                     msg: msg, 
-                    message: message,
                     err: err
-                }); */
-            });            
+                }); 
+            });  
+            */    
+                var source = req.userDisplay.type == 'other' ? 'CSIL Account' : 'SFU Central Authentication Service';
+                var allowAdmin = req.userDisplay.allowAdmin;
+                res.render('comment', {
+                username: username,
+                source: source,
+                allowAdmin: allowAdmin,
+                page: "Report Violations Send Feedback",
+                csrfToken: req.csrfToken(), 
+                msg: msg, 
+                err: err
+            });       
         }
     });
 
